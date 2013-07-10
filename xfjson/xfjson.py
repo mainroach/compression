c83aa0158727ca7090016e7cf3e660b668f0ca3a
mainroach <mainroach@gmail.com>
2013-07-10 11:17:01 -0700

    some refactoring

--- xfjson/xfjson.py
+++ xfjson/xfjson.py
@@ -1,21 +1,13 @@
 # This Python file uses the following encoding: utf-8
 
-import urllib
-import xml.dom.minidom as minidom
-import subprocess
-import os.path
+
 import json
-import logging
-import csv
+import struct
 import sys
-import datetime
 import math
-from subprocess import call
 import shutil, errno
-from subprocess import Popen
 import operator
 
-import byteStream
 
 '''
 We transpose a JSON format from ‘array of structs’ to ‘struct of arrays’ and yield benefits 
@@ -39,6 +31,114 @@ COMPRESSION
 DECOMPRESSION
     -d - Do decompression. Note, all the information needed to decompress the variants of the file are included in the file. You simply need to call 'decompress'
 '''
+
+
+
+#===================================
+class ByteStreamWriter(object):
+    _array=bytes()
+    _endianMode="" #"" = don't care, "<" little endian, ">" big endian
+    #http://docs.python.org/3.0/library/struct.html
+    def writeString(self,strng,writeLen=True):
+        if isinstance(strng, unicode):
+            strng = strng.encode("ascii","ignore")
+            
+        ln = len(strng)
+        if(writeLen):
+            if ln > 0x80000:
+                print "Data to big to encode this way"
+            self.writeUShort(ln)
+        self._array += struct.pack(str(ln) +"s",bytes(strng))
+
+
+
+    def writeByte(self,val):
+        self._array += struct.pack(self._endianMode+"b",val)
+
+    def writeUByte(self,val):
+        self._array += struct.pack(self._endianMode+"B",val)
+
+    def writeShort(self,val):
+        self._array += struct.pack(self._endianMode+"h",val)
+
+    def writeUShort(self,val):
+        self._array += struct.pack(self._endianMode+"H",val)
+
+    def writeInt(self,val):
+        self._array += struct.pack(self._endianMode+"i",val)
+        
+    def writeUInt(self,val):
+        self._array += struct.pack(self._endianMode+"I",val)
+
+    def writeLong(self,val):
+        self._array += struct.pack(self._endianMode+"q",val)
+        
+    def writeULong(self,val):
+        self._array += struct.pack(self._endianMode+"Q",val)
+
+    def writeFloat(self,val):
+        self._array += struct.pack(self._endianMode+"f",val)
+#===================================
+class ByteStreamReader(object):
+    _pos =0
+    _array=bytes()
+    _endianMode="" #"" = don't care, "<" little endian, ">" big endian
+
+    def readByte(self):
+        v= struct.unpack_from(self._endianMode+"b",self._array[self._pos])[0]
+        self._pos+=1
+        return v
+
+    def readUByte(self):
+        v= struct.unpack_from(self._endianMode+"B",self._array[self._pos])[0]
+        self._pos+=1
+        return v
+
+    def readString(self,readLen=0):
+
+        if readLen ==0:
+            readLen = self.readUShort()
+        v = struct.unpack_from(str(readLen) + "s",self._array[self._pos : self._pos+readLen])[0]
+        self._pos+=readLen
+        return v
+
+    def readShort(self):
+        v = struct.unpack_from(self._endianMode+"h",self._array[self._pos:self._pos+2])[0]
+        self._pos+=2
+        return v
+
+    def readUShort(self):
+        v = struct.unpack_from(self._endianMode+"H",self._array[self._pos:self._pos+2])[0]
+        self._pos+=2
+        return v
+
+    def readInt(self):
+        v = struct.unpack_from(self._endianMode+"i",self._array[self._pos:self._pos+4])[0]
+        self._pos+=4
+        return v
+
+    def readUInt(self):
+        v = struct.unpack_from(self._endianMode+"I",self._array[self._pos:self._pos+4])[0]
+        self._pos+=4
+        return v
+
+    def readLong(self):
+        v = struct.unpack_from(self._endianMode+"q",self._array[self._pos:self._pos+8])[0]
+        self._pos+=4
+        return v
+
+    def readULong(self):
+        v = struct.unpack_from(self._endianMode+"Q",self._array[self._pos:self._pos+8])[0]
+        self._pos+=4
+        return v
+    def readFloat(self):
+        v = struct.unpack_from(self._endianMode+"f",self._array[self._pos:self._pos+4])[0]
+        self._pos+=4
+        return v
+
+    def eos(self):
+        return self._pos == len(self._array)
+
 class TransposeJSON(object):
     _tranposeMode = 0
     #=========================================
@@ -206,7 +306,7 @@ class BinaryJSON(object):
     #=========================================
     #we will only pack a list of dictionary objects that do not have child lists, of dictionaries in them...
     def encode(self,jsonObj):
-        self._binaryStream = byteStream.ByteStreamWriter()                    
+        self._binaryStream = ByteStreamWriter()                    
         self._binaryStream.writeUByte(self._itemToken)
         self._binaryStream.writeUByte(0x00)#flags bit, we'll set this later
 
@@ -276,7 +376,7 @@ class BinaryJSON(object):
     _output=""
     #=========================================
     def decode(self,bytes):
-        self._binaryStream = byteStream.ByteStreamReader()
+        self._binaryStream = ByteStreamReader()
         self._binaryStream._array = bytes
         startByte = self._binaryStream.readUByte() #read our keystone byte
         flagsByte = self._binaryStream.readUByte() #read our flags
@@ -397,11 +497,7 @@ def encodeJSON(inFile,outFile, dataTransposeMode,doBinPack):
         jsdata = f.read()
         f.close()
 
-    
-    #clean up the JSON data a little bit; for example, JSONP information...
-    #technically, we shouldn't strip JSONP, we should just view it as a string.
-    jsdata = stripJSONP(jsdata)
-    
+
     #parse the JSON object
     jsonObj = None
     try:
